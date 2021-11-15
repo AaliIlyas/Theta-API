ca2d38b258dfc92fe0d087a00c3988260f544910
Ilyas <aali.ilyas1@gmail.com>
2021-11-15 23:46:23 +0000

    Initial commit, Db models, Context, Sql Server

---
+++ Models/Database/Customers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Theta.Models.Database
+{
+    public class Customers
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Orders> Orders { get; set; }
+    }
+}
---
+++ RetailContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Theta.Models.Database;
+
+namespace Bookish.DbModels
+{
+    public class RetailContext : DbContext
+    {
+        public RetailContext(DbContextOptions<RetailContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Products> Members { get; set; }
+        public DbSet<Orders> CheckedOutBooks { get; set; }
+    }
+}
